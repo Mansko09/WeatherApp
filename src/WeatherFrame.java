621496cfe0d6c3c2d428d1a73e614f9ee3bd794b
Mansko09 <127218021+Mansko09@users.noreply.github.com>
2024-02-14 21:45:32 +0100

    created frame

--- src/WeatherFrame.java
+++ src/WeatherFrame.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class WeatherFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class WeatherFrame extends JFrame{
+    WeatherFrame() {
+        ImageIcon image = new ImageIcon("WeatherApp_BG.png");
+        JLabel label = new JLabel();
+        label.setIcon(image);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        this.setTitle(" Weather App ");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(400,800);
+        this.setVisible(true);
+        this.getContentPane().setBackground(Color.pink);
+    }
 }
